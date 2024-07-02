88f343ff14fec419bf02db31fb5a4e804e7c3e24
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-07-03 00:59:08 +0530

    Daily DSA

---
+++ checkPalindromRec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool checkPalindrome(string s, int i, int j) {
+
+  // base case
+  if (i > j) {
+    return true;
+  }
+
+  if (s[i] != s[j]) {
+    return false;
+  } else {
+    return checkPalindrome(s, i + 1, j - 1);
+  }
+}
+
+int main(int argc, char *argv[]) { return 0; }
